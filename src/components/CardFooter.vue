b6e0bcae6345621629ae0fdeeb019a3ca054e0d8
‪Mohamed Elzorkany <86691917+Nano232@users.noreply.github.com>
2023-10-14 18:46:52 +0300

    updates

---
+++ src/components/CardFooter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto" />
+    <div class="text-center">
+      <a
+        href="https://www.linkedin.com/in/mohamed-elzorkany-24b97b221/"
+        target="_blank"
+        class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"
+        ><font-awesome-icon icon="fa-brands fa-linkedin"
+      /></a>
+      <a
+        href="https://www.facebook.com/mohamed2320182233"
+        target="_blank"
+        class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"
+        ><font-awesome-icon icon="fa-brands fa-facebook"
+      /></a>
+      <a
+        href="https://www.instagram.com/m_elzorkany2/"
+        target="_blank"
+        class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"
+        ><font-awesome-icon icon="fa-brands fa-instagram"
+      /></a>
+      <a
+        href="https://github.com/Nano232"
+        target="_blank"
+        class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"
+      >
+        <font-awesome-icon icon="fa-brands fa-github"
+      /></a>
+      <a
+        href="mailto:elzorany.mohamed232@gmail.com"
+        class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"
+      >
+        <font-awesome-icon icon="fa-solid fa-envelope"
+      /></a>
+    </div>
+  </div>
+</template>
+<script setup></script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center">
+    <h2
+      class="text-center text-xl sm:text-2xl font-bold uppercase dark:text-white"
+    >
+      Mohamed Elzorkany
+    </h2>
+    <p
+      class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto dark:text-gray-300"
+    >
+      Frontend developer Vue.js with a knowledge of modern web development
+      technologies. Skilled in HTML, CSS, Bootstrap, Tailwind CSS, TypeScript,
+      JavaScript, Vue.js (including Option API and Composition API), Git,
+      GitHub, and Axios
+    </p>
+  </div>
+</template>
+<script setup></script>
